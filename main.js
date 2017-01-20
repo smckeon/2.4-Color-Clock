d347d1f199900fa8cbbd83db479169e2b9a98866
smckeon <mckeon.sean@outlook.com>
2017-01-20 16:14:34 -0500

    maddy push

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 (function(){
-	“use strict”;
+	// “use strict”;
 
 
-  
+
+function updateTime() {
+  var time = new Date();
+  var seconds = time.getSeconds();
+  var secondsContent = document.querySelector(".elapsed-time .seconds")
+  secondsContent.textContent = seconds;
+}
+
+window.setInterval(updateTime, 1000)
+
+
 }());
