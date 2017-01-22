85eaeafa3efb8667eb6a3712d9528303fd808a73
smckeon <mckeon.sean@outlook.com>
2017-01-21 21:32:46 -0500

    init2

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 (function(){
 	// “use strict”;
 
-
-
 function updateTime() {
   var time = new Date();
   var seconds = time.getSeconds();
-  var secondsContent = document.querySelector(".elapsed-time .seconds")
+  var secondsContent = document.querySelector(".elapsed-time .seconds");
   secondsContent.textContent = seconds;
+
+  var minutes = time.getMinutes();
+  var minutesContent = document.querySelector(".elapsed-time .minutes");
+  minutesContent.textContent = minutes;
+
+  var hours = time.getHours();
+  var hoursContent = document.querySelector(".elapsed-time .hours");
+  hoursContent.textContent = hours;
+
+
+
+
+
+
+
+
+
+
+
+  console.log('You finally did it!')
+
 }
 
 window.setInterval(updateTime, 1000)
-
 
 }());
